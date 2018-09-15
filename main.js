0b0e8a40156fb6da75384eb2cff12b677f3806c7
svd2802 <narutoche@bk.ru>
2018-09-15 12:45:10 +0600

    15.09.2018 Пара ver 1.0

---
+++ main.js
@@ -0,0 +1,25 @@
+var someData= "Datesad";
+
+if(true) {
+    let localData="localdsaaswed";
+    const Data="contant";
+}
+
+const globalArray = [];
+globalArray.push(312);
+let index= globalArray.indexOf(312);
+
+let obj={
+    id: 123,
+    name: 'asdasd',
+    title: "asdasd"
+};
+
+obj.addField = 123;
+
+globalArray.push(obj);
+
+function onButtonClick() {
+    let el= document.getElementById('t2');
+    el.innerText="aghjhlkbnlasda";
+}
